c773983ea224631de3e1a0143d84b50ae8bb7802
UsamaShehbaz <132691431+UsamaShehbaz@users.noreply.github.com>
2023-06-03 00:44:36 +0500

    back button include,password validation on forgot password done, student teacher and admin logout ok

--- student/login.php
+++ student/login.php
@@ -1,45 +1,45 @@
 <?php
-  // session_start();
-  include '../includes/studentHeader.php';
+// session_start();
+include '../includes/studentHeader.php';
 
-  $success = $_SESSION['success'] ?? '';
-  $error = $_SESSION['error'] ?? '';
-  unset($_SESSION['success']);
-  unset($_SESSION['error']);
+$success = $_SESSION['success'] ?? '';
+$error = $_SESSION['error'] ?? '';
+unset($_SESSION['success']);
+unset($_SESSION['error']);
 
-  if (isset($_POST['login'])) {
-    $email = $_POST['email'];
+if (isset($_POST['login'])) {
+  $email = $_POST['email'];
 
-    //Also we can use *Roll-No* for login
-    $sql = "SELECT id, password FROM student WHERE email='".$email."' ";
-    $result = mysqli_query($conn, $sql);
-    if (mysqli_num_rows($result) > 0) {
-      $row = mysqli_fetch_array($result);
-      $id = $row['id'];
-      $password = $row['password'];
+  //Also we can use *Roll-No* for login
+  $sql = "SELECT id, password FROM student WHERE email='" . $email . "' ";
+  $result = mysqli_query($conn, $sql);
+  if (mysqli_num_rows($result) > 0) {
+    $row = mysqli_fetch_array($result);
+    $id = $row['id'];
+    $password = $row['password'];
 
-      if (password_verify($_POST['password'], $password)) {
-        session_regenerate_id();
-        $_SESSION['loggedin'] = TRUE;
-        $_SESSION['email'] = $_POST['email'];
-        $_SESSION['id'] = $id;
+    if (password_verify($_POST['password'], $password)) {
+      session_regenerate_id();
+      $_SESSION['loggedin'] = TRUE;
+      $_SESSION['email'] = $_POST['email'];
+      $_SESSION['id'] = $id;
 
-        header("Location: student.php?student_id=$id");
-      } else {
-        $_SESSION['error'] = "Incorrect email and/or password!";
-        header("Location: login.php");
-      }
-      
+      header("Location: student.php?student_id=$id");
     } else {
-      $_SESSION['error'] = "User does not exist!";
+      $_SESSION['error'] = "Incorrect email and/or password!";
       header("Location: login.php");
     }
+
+  } else {
+    $_SESSION['error'] = "User does not exist!";
+    header("Location: login.php");
   }
+}
 ?>
 
 
 <body>
-  
+
   <div class="container">
     <div class="alert alert-success alert-dismissible <?php echo !empty($success) ? 'd-block' : 'd-none'; ?>">
       <button type="button" class="close" data-dismiss="alert">&times;</button>
@@ -52,7 +52,7 @@
 
     <div class="login">
       <h2 class="font-weight-bold text-center py-2">Student</h2>
-      <form action="<?php  echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+      <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
         <div class="row justify-content-center pt-2">
           <div class="col-md-4">
             <input type="email" class="form-control myborder px-3 py-1" name="email" placeholder="Email" required>
@@ -61,7 +61,8 @@
 
         <div class="row justify-content-center pt-3">
           <div class="col-md-4">
-            <input type="password" class="form-control myborder px-3 py-1" name="password" placeholder="Password" required>
+            <input type="password" class="form-control myborder px-3 py-1" name="password" placeholder="Password"
+              required>
           </div>
         </div>
 
@@ -73,7 +74,9 @@
       </form>
 
       <div class="text-center px-4 pt-3">
-        <a href="forgotPassword.php" class="font-weight-bold font">Forgot Password ?</a>
+        <a href="forgotPassword.php" class="font-weight-bold font">Forgot Password ?</a>|
+        <a href="http://localhost/face-attendance-system/" class="font-weight-bold font"
+          style="margin-left: 5px;">Back</a>
       </div>
     </div>
   </div>
@@ -82,4 +85,5 @@
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
--- teacher/signin.php
+++ teacher/signin.php
@@ -1,53 +1,53 @@
 <?php
-  session_start();
-  include '../includes/teacherHeader.php';
+session_start();
+include '../includes/teacherHeader.php';
 
-  $success = $_SESSION['success'] ?? '';
-  $error = $_SESSION['error'] ?? '';
-  unset($_SESSION['success']);
-  unset($_SESSION['error']);
+$success = $_SESSION['success'] ?? '';
+$error = $_SESSION['error'] ?? '';
+unset($_SESSION['success']);
+unset($_SESSION['error']);
 
-  if (isset($_POST['login'])) {
-    $email = $_POST['email'];
+if (isset($_POST['login'])) {
+  $email = $_POST['email'];
 
-    $sql = "SELECT * FROM teacher WHERE email='".$email."' ";
-    $result = mysqli_query($conn, $sql);
-    if (mysqli_num_rows($result) > 0) {
-      $row = mysqli_fetch_array($result);
-      $id = $row['id'];
-      $password = $row['password'];
+  $sql = "SELECT * FROM teacher WHERE email='" . $email . "' ";
+  $result = mysqli_query($conn, $sql);
+  if (mysqli_num_rows($result) > 0) {
+    $row = mysqli_fetch_array($result);
+    $id = $row['id'];
+    $password = $row['password'];
 
-      if (password_verify($_POST['password'], $password)) {
-        session_regenerate_id();
-        $_SESSION['teacher_login'] = TRUE;
-        $_SESSION['teacher_email'] = $_POST['email'];
-        $_SESSION['teacher_id'] = $id;
+    if (password_verify($_POST['password'], $password)) {
+      session_regenerate_id();
+      $_SESSION['teacher_login'] = TRUE;
+      $_SESSION['teacher_email'] = $_POST['email'];
+      $_SESSION['teacher_id'] = $id;
 
-        header("Location: timetable.php?teacher_id=$id");
-      } else {
-        $_SESSION['error'] = "Incorrect email and/or password!";
-        header("Location: signin.php");
-      }
-
+      header("Location: timetable.php?teacher_id=$id");
     } else {
-      $_SESSION['error'] = "User does not exist!";
+      $_SESSION['error'] = "Incorrect email and/or password!";
       header("Location: signin.php");
     }
 
-
-
-    // if (mysqli_num_rows($result) > 0) {
-    //   $row = mysqli_fetch_array($result);
-    //   $id = $row['id'];
-    //   $password = $row['password'];
-    //   header("Location: timetable.php?teacher_id=$id");
-    // } else {
-    //   echo "<div class='alert alert-danger alert-dismissible'>
-    //     <button type='button' class='close' data-dismiss='alert'>&times;</button>
-    //     User does not exist!
-    //   </div>";
-    // }
+  } else {
+    $_SESSION['error'] = "User does not exist!";
+    header("Location: signin.php");
   }
+
+
+
+  // if (mysqli_num_rows($result) > 0) {
+  //   $row = mysqli_fetch_array($result);
+  //   $id = $row['id'];
+  //   $password = $row['password'];
+  //   header("Location: timetable.php?teacher_id=$id");
+  // } else {
+  //   echo "<div class='alert alert-danger alert-dismissible'>
+  //     <button type='button' class='close' data-dismiss='alert'>&times;</button>
+  //     User does not exist!
+  //   </div>";
+  // }
+}
 ?>
 
 <body>
@@ -63,7 +63,7 @@
 
     <div class="login">
       <h2 class="font-weight-bold text-center py-2">Teacher</h2>
-      <form action="<?php  echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+      <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
         <div class="row justify-content-center pt-2">
           <div class="col-md-4">
             <input type="email" class="form-control myborder px-3 py-1" name="email" placeholder="Email" required>
@@ -72,7 +72,8 @@
 
         <div class="row justify-content-center pt-3">
           <div class="col-md-4">
-            <input type="password" class="form-control myborder px-3 py-1" name="password" placeholder="Password" required>
+            <input type="password" class="form-control myborder px-3 py-1" name="password" placeholder="Password"
+              required>
           </div>
         </div>
 
@@ -84,7 +85,9 @@
       </form>
 
       <div class="text-center px-4 pt-3">
-        <a href="forgotpassword.php" class="font-weight-bold font">Forgot Password ?</a>
+        <a href="forgotpassword.php" class="font-weight-bold font">Forgot Password ?</a>|
+        <a href="http://localhost/face-attendance-system/" class="font-weight-bold font"
+          style="margin-left: 5px;">Back</a>
       </div>
     </div>
   </div>
@@ -93,4 +96,5 @@
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
